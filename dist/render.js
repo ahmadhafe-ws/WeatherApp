30991c9ffd82860098ae8bca7bc1c1f0ff85ff0b
Afeef <afeef.as11@gmail.com>
2021-06-09 11:44:45 +0300

    Weather App

---
+++ dist/render.js
@@ -0,0 +1,15 @@
+
+const citiesCards = $(".citiesCards")
+
+class Renderer {
+    constructor() {}
+
+    renderData = function (cities) {
+        citiesCards.empty()
+        
+        const source = $('#city-template').html();
+        const template = Handlebars.compile(source);
+        const newHTML = template({cities});
+        citiesCards.append(newHTML);
+    }
+}
